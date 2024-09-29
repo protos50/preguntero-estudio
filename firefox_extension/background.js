cb90df7dd6d9c80de98e721c6cb9dd6957229d8c
francojzini <francojzini@gmail.com>
2024-09-28 22:19:27 -0300

    extension edit

--- firefox_extension/background.js
+++ firefox_extension/background.js
@@ -1,23 +1,23 @@
-// Crear el menú contextual para la selección de texto
-browser.menus.create({
-    id: "sendToSearch",
-    title: "Enviar texto a mi buscador",
-    contexts: ["selection"]
-  });
-  
-  // Cuando se selecciona la opción en el menú contextual
-  browser.menus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "sendToSearch") {
+chrome.contextMenus.create({
+  id: "sendToSearch",
+  title: "Ask a question",
+  contexts: ["selection"]
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "sendToSearch") {
       // Buscar si la pestaña de tu aplicación ya está abierta
-      browser.tabs.query({ url: "http://localhost:5000/*" }, (tabs) => {
-        if (tabs.length > 0) {
-          // Si está abierta, enviar un mensaje a la pestaña con el texto seleccionado
-          browser.tabs.sendMessage(tabs[0].id, { action: "searchText", text: info.selectionText });
-        } else {
-          // Si no está abierta, abrir una nueva pestaña con el texto seleccionado
-          browser.tabs.create({ url: "http://localhost:5000?question=" + encodeURIComponent(info.selectionText) });
-        }
+      chrome.tabs.query({ url: "http://localhost:5000/*" }, (tabs) => {
+          if (tabs.length > 0) {
+              // Si está abierta, enviar un mensaje a la pestaña con el texto seleccionado
+              chrome.tabs.sendMessage(tabs[0].id, { action: "searchText", text: info.selectionText });
+          } else {
+              // Si no está abierta, abrir una nueva pestaña con el texto seleccionado
+              chrome.tabs.create({ url: "http://localhost:5000?question=" + encodeURIComponent(info.selectionText) });
+          }
       });
-    }
-  });
-  
+  }
+});
+
+
+
